a223575e4ba9ab292f397ba555050808c8d0d415
techatikin <nikitakhabya03@gmail.com>
2023-05-26 11:11:03 +0530

    feat: modified readme, minor refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,34 @@ import { careersLoader } from './pages/careers/Careers'
 import { careerDetailsLoader } from './pages/careers/CareerDetails'
 import { contactAction } from './pages/help/Contact'
 
+// Create the router using createBrowserRouter and createRoutesFromElements
 const router = createBrowserRouter(
   createRoutesFromElements(
+    // Root layout
     <Route path='/' element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path='/about' element={<About />} />
+      <Route index element={<Home />} /> // Home page
 
+      <Route path='/about' element={<About />} /> // About page
+
+      {/* Help layout */}
       <Route path='help' element={<HelpLayout />}>
-        <Route path='faq' element={<FAQ />} />
-        <Route path='contact' element={<Contact />} action={contactAction} />
+        <Route path='faq' element={<FAQ />} /> // FAQ page
+        <Route path='contact' element={<Contact />} action={contactAction} /> // Contact page with action
       </Route>
 
+      {/* Careers layout */}
       <Route path='careers' element={<CareersLayout />} errorElement={<CareersError />}>
-        <Route
-          index
-          element={<Careers />}
-          loader={careersLoader}
+        <Route index element={<Careers />} loader={careersLoader} /> // Careers page with loader
 
-        />
-        <Route path=':id'
+        {/* Career detail page */}
+        <Route
+          path=':id'
           element={<CareerDetail />}
           loader={careerDetailsLoader}
-
         />
       </Route>
 
-      <Route path='*' element={<NotFound />}></Route>
+      <Route path='*' element={<NotFound />} /> // Not found page
     </Route>
   )
 )
